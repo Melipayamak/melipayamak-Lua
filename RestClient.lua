1a9b3cf25310f8acc40ab1e4d8baeeaee1530350
Amirhossein Mehrvarzi <36813422+triggergroup@users.noreply.github.com>
2018-07-13 13:32:00 +0430

    Create RestClient.lua

---
+++ RestClient.lua
@@ -0,0 +1,50 @@
+local http = require("socket.http")
+local ltn12 = require("ltn12")
+
+
+function makeRequest( URL, data )
+	local response = {}
+	http.request({
+	    method = "POST",
+	    url = URL,
+	    headers = {
+	            ["Content-Type"] = "application/x-www-form-urlencoded",
+	            ["Content-Length"] = string.len(data)
+	        },
+	    source = ltn12.source.string(data),
+	    sink = ltn12.sink.table(response)
+	    })
+	print(table.concat(response))
+end
+
+
+function SendSMS( username, password, from, to, text )
+	local data =  "username="..username.."&password="..password.."&from="..from.."&to="..to.."&text="..text
+	makeRequest("https://rest.payamak-panel.com/api/SendSMS/SendSMS", data)
+end
+
+
+function GetDeliveries2( username, password, recId )
+	local data =  "UserName="..username.."&PassWord="..password.."&recId="..recId
+	makeRequest("https://rest.payamak-panel.com/api/SendSMS/GetDeliveries2", data)
+end
+
+function GetMessages( username, password, location, from, index, count )
+	local data =  "UserName="..username.."&PassWord="..password.."&Location="..location.."&From="..from.."&Index="..index.."&Count="..count
+	makeRequest("https://rest.payamak-panel.com/api/SendSMS/GetMessages", data)
+end
+
+function GetCredit( username, password )
+	local data =  "UserName="..username.."&PassWord="..password
+	makeRequest("https://rest.payamak-panel.com/api/SendSMS/GetCredit", data)
+end
+
+function GetBasePrice( username, password )
+	local data =  "UserName="..username.."&PassWord="..password
+	makeRequest("https://rest.payamak-panel.com/api/SendSMS/GetBasePrice", data)
+end
+
+function GetUserNumbers( username, password )
+	local data =  "UserName="..username.."&PassWord="..password
+	makeRequest("https://rest.payamak-panel.com/api/SendSMS/GetUserNumbers", data)
+end
